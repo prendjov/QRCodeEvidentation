aad1c99ca39201ebb87b19edb9f36e7b811a3f18
Van40s <vanestojanov49@gmail.com>
2024-07-28 14:09:56 +0200

    LectureEditDto class

---
+++ QRCodeEvidentationApp/Models/DTO/LectureEditDto.cs
@@ -0,0 +1,26 @@
+namespace QRCodeEvidentationApp.Models.DTO;
+
+public class LectureEditDto
+{
+    public Lecture lecture { get; set; }
+    public string lectureId { get; set; }
+    
+    public List<string> Type = new List<string>()
+    {
+        "Аудиториски", "Предавања"
+    };
+    
+    public List<Room>? GetAvailableRooms { get; set; }
+    
+    public List<CourseProfessor>? CoursesProfessor { get; set; }
+    
+    public List<CourseAssistant>? CoursesAssistant { get; set; }
+    
+    public long? CourseId { get; set; }
+    
+    public string? CourseProfessorId { get; set; }
+    
+    public string? CourseAssistantId { get; set; }
+
+    public string? ErrMessage { get; set; }
+}
